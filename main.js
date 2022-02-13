508332d0adadbaf266d69b49e2601692845e38c8
freeBoy <tohirbabs@gmail.com>
2022-02-13 01:37:09 +0100

    update
    update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle')
 const navList = document.querySelector('.nav-list')
+const navItems = document.querySelectorAll('.nav-item')
 const scrollProgress = document.querySelector('.scroll-progress')
 
 navToggle.addEventListener('click', () => {
@@ -7,12 +8,31 @@ navToggle.addEventListener('click', () => {
 
     if(navShown){
         navList.classList.remove('show-nav')
+        navList.classList.add('hide-nav')
     }
 
     else{
         navList.classList.add('show-nav')
+        navList.classList.remove('hide-nav')
     }
 })
+
+navItems.forEach(navItem => {
+    navItem.addEventListener('click', () => {
+        const navShown =  navList.classList.contains('show-nav')
+    
+        if(navShown){
+            navList.classList.remove('show-nav')
+            navList.classList.add('hide-nav')
+        }
+    
+        else{
+            navList.classList.add('show-nav')
+            navList.classList.remove('hide-nav')
+        }
+    })
+        
+});
 
 window.addEventListener('scroll', () => {
     const scrollable = document.documentElement.scrollHeight - window.innerHeight
